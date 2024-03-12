0fd95a73d0dc0ac3e1ae2de2cad63b10d7f5ac0f
sachinisiriwardene <sachinisiriwardene@gmail.com>
2024-03-12 11:53:55 +0530

    fetch api content from a markdown

--- src/pages/[orgName]/api/[apiName].js
+++ src/pages/[orgName]/api/[apiName].js
@@ -1,6 +1,9 @@
 import Navbar from '../../../app/Navbar';
 import Footer from '../../../app/Footer';
 import { Helmet } from "react-helmet";
+import Markdown from 'react-markdown'
+import { createRoot } from 'react-dom/client'
+import rehypeRaw from 'rehype-raw'
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { LoadCSS } from '../../util';
@@ -10,12 +13,15 @@ export async function getServerSideProps(context) {
     var htmlRef;
     var stylesheetRef;
     var apiContentRef;
+    var apiContentRefMD;
     var yamlRef;
 
     if (process.env.NEXT_PUBLIC_DEPLOYMENT === "DEV") {
         htmlRef = process.env.NEXT_PUBLIC_HOST + "resources/template/api-landing-page.html"
         stylesheetRef = process.env.NEXT_PUBLIC_HOST + "resources/stylesheet/api-landing-page.css";
         apiContentRef = process.env.NEXT_PUBLIC_HOST + "resources/content/apiMedatada.json";
+        apiContentRefMD = process.env.NEXT_PUBLIC_HOST + "resources/content/apiContent.md";
+
         yamlRef = process.env.NEXT_PUBLIC_HOST + "resources/content/theme.json"
 
     } else {
@@ -35,6 +41,10 @@ export async function getServerSideProps(context) {
     const resp = await fetch(apiContentRef);
     const apiContent = await resp.json();
     content.apiArtifacts = apiContent.apiInfo.apiArtifacts;
+
+    const apiMDRes = await fetch(apiContentRefMD);
+    const apiPageContent = await apiMDRes.text();
+    content.apiPage = apiPageContent;
 
     const respo = await fetch(stylesheetRef);
     const stylesheetContent = await respo.text();
@@ -64,6 +74,9 @@ function API({ content }) {
                 apiImage.src = value;
             }
         }
+
+        //render rest of the API Landin Page content through a markdown
+        createRoot(document.getElementById("api-details")).render( <Markdown rehypePlugins={[rehypeRaw]}>{content.apiPage}</Markdown> );
 
         const styleElement = document.createElement('style');
         styleElement.innerHTML = content.stylesheetContent;
