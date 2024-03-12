b04044dcc194e0e16096efbc1c3c724c2862d1dd
Divya Premanantha <premananthadivya@gmail.com>
2024-03-12 14:59:52 +0530

    Add executable

--- src/middleware.js
+++ src/middleware.js
@@ -2,8 +2,8 @@ import { NextResponse } from 'next/server'
 
 export async function middleware(request) {
     if (process.env.NEXT_PUBLIC_DEPLOYMENT === "DEV") {
-        return NextResponse.redirect(new URL(process.env.NEXT_PUBLIC_HOST + request.nextUrl.pathname));
-        // return NextResponse.next();
+        // return NextResponse.redirect(new URL(process.env.NEXT_PUBLIC_HOST + request.nextUrl.pathname));
+        return NextResponse.next();
     }
 
     if (request.headers.get('referer') !== null) {
