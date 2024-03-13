c1cedfceb556d54f799394c4f4734e789c9f2fd3
Divya Premanantha <premananthadivya@gmail.com>
2024-03-13 15:35:43 +0530

    Fix content loader

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, options) => {
+        config.resolve.fallback = { fs: false };
+        config.module.rules.push({
+            test: /\.(js|jsx)$/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-react'],
+                },
+            }
+        },
+        )
+        return config
+    },
+}
 
 export default nextConfig;
