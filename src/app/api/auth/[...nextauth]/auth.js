1083613a841e8e0eee932f8dde947f96254f4280
Divya Premanantha <premananthadivya@gmail.com>
2024-04-22 16:07:21 +0530

    Remove additional console logs

--- src/app/api/auth/[...nextauth]/auth.js
+++ src/app/api/auth/[...nextauth]/auth.js
@@ -3,7 +3,6 @@ import { authConfig } from './authConfig';
 import { getProviders } from '../../../../pages/signIn';
 
 const providers = getProviders();
-console.log(providers);
 
 auth.profile = (profile) => {
   return { role: profile.role ?? "user" }
@@ -12,12 +11,9 @@ auth.profile = (profile) => {
 export const { handlers: { GET, POST },
   auth, signIn, signOut } = NextAuth({
     ...authConfig,
-
     providers: providers,
     callbacks: {
       jwt({ token, user }) {
-        console.log("roles")
-        console.log(user)
         if (user) token.role = user.role
         return token
       },
