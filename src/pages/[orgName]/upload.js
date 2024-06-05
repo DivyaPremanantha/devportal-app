9eead63bc9b756e2746b847f36a94ffb99cb63bb
Divya Premanantha <premananthadivya@gmail.com>
2024-06-05 08:58:50 +0530

    Add zip generation logic

--- src/pages/[orgName]/upload.js
+++ src/pages/[orgName]/upload.js
@@ -1,12 +1,57 @@
 import React, { useState } from 'react';
 import './upload.css';
+import JSZip from 'jszip';
+import { saveAs } from 'file-saver';
+import { promises as fs } from 'fs';
+import { useEffect } from "react";
+
+
+
+export async function getServerSideProps(context) {
+    var content = {}
+    console.log('Generating zip file...');
+
+    var file_system = require('fs');
+    var archiver = require('archiver');
+
+    // var output = file_system.createWriteStream(context.params.orgName + '.zip');
+    var output = file_system.createWriteStream("./public/test" + '.zip');
+
+    var archive = archiver('zip');
+
+    output.on('close', function () {
+        console.log(archive.pointer() + ' total bytes');
+        console.log('archiver has been finalized and the output file descriptor has closed.');
+    });
+
+    archive.on('error', function (err) {
+        throw err;
+    });
+
+    archive.pipe(output);
+
+    // append files from a sub-directory, putting its contents at the root of archive
+    archive.directory(process.cwd() + "/../../public", false);
+
+    // append files from a sub-directory and naming it `new-subdir` within the archive
+    archive.directory('subdir/', 'new-subdir');
+
+    archive.finalize();
+    content.orgName = context.params.orgName;
+    // content.test = await fs.readFile('MNMTaxiSolution.zip', 'utf8');
+
+    // console.log('test:', content.test);
+    return { props: { content } }
+
+}
+
 
 export default function Upload({ content }) {
+
     // Define state variables for each input
     const [orgName, setOrgName] = useState('');
     const [isPublic, setIsPublic] = useState(false);
     const [authenticatedPages, setAuthenticatedPages] = useState('');
-    const [response, setResponse] = useState(null);
 
     // Handle form submission
     const handleSubmit = async (event) => {
@@ -18,16 +63,35 @@ export default function Upload({ content }) {
         };
 
         try {
-            const response = await fetch(process.env.NEXT_PUBLIC_ADMIN_API_URL + 'organisation', {
+            const orgResponse = await fetch(process.env.NEXT_PUBLIC_ADMIN_API_URL + 'organisation', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(formData),
             });
-            const result = await response.json();
-            setResponse(result);
-            console.log('Form Data Submitted:', result);
+            const result = await orgResponse.json();
+            if (!orgResponse.ok) {
+                throw new Error('Failed to submit form data');
+            } else {
+
+                const zipResponse = await fetch("/" + formData.orgName + '.zip');
+
+                if (!zipResponse.ok) {
+                    throw new Error('Failed to fetch zip file from local repository');
+                }
+                const zipArrayBuffer = await zipResponse.arrayBuffer();
+                const zipBlob = new Blob([zipArrayBuffer], { type: 'application/zip' });
+                const orgContentResponse = await fetch(process.env.NEXT_PUBLIC_ADMIN_API_URL + 'orgContent?orgName=' + formData.orgName, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/zip',
+                    },
+                    body: zipBlob,
+                });
+                await orgContentResponse.json();
+                console.log('Form Data Submitted:', result);
+            }
         } catch (error) {
             console.error('Error submitting form:', error);
         }
